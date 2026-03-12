d66c95cbeaba45af71bec963f2bc81015e24a28f
Caner Yilmaz <caneryilmaz57@gmail.com>
2026-03-12 15:06:03 +0300

    Comments added

--- custom_classes.py
+++ custom_classes.py
@@ -1,70 +1,70 @@
-class MediaItem:
-    def __init__(self,title,author,is_available=True):
-        self.title = title
-        self.author = author
-        self.is_available = is_available
+class MediaItem:   # Base class for library items
+    def __init__(self,title,author,is_available=True):   # Constructor method
+        self.title = title   # Stores the title
+        self.author = author   # Stores the author
+        self.is_available = is_available   # Stores availability
     
-    def checkout(self):
-        if self.is_available:
-            self.is_available = False
-            print("Successfull checkout\n")
-        else:
-            print("Already out\n")
+    def checkout(self):   # Method for borrowing the item
+        if self.is_available:   # Checks if the item is available
+            self.is_available = False   # Marks the item as checked out
+            print("Successfull checkout\n")   # Prints success message
+        else:   # If the item is not available
+            print("Already out\n")   # Prints warning message
 
-    def return_item(self):
-        self.is_available = True
-        print("Item returned\n")
+    def return_item(self):   # Method for returning the item
+        self.is_available = True   # Marks the item as available
+        print("Item returned\n")   # Prints return message
 
-    def __str__(self):
-        if self.is_available:
-            status = "Available"
-        else:
-            status = "Checked out"
+    def __str__(self):   # String representation of the object
+        if self.is_available:   # Checks availability
+            status = "Available"   # Sets status as Available
+        else:   # If not available
+            status = "Checked out"   # Sets status as Checked out
         
-        return self.title + " by " + self.author + " [" + status + "]"
+        return self.title + " by " + self.author + " [" + status + "]"   # Returns item info
 
-class Book(MediaItem):
-    def __init__(self,title,author,page_count,is_available=True):
-        self.title = title
-        self.author = author
-        self.is_available = is_available
-        self.page_count = page_count
+class Book(MediaItem):   # Subclass for books
+    def __init__(self,title,author,page_count,is_available=True):   # Constructor for Book
+        self.title = title   # Stores the book title
+        self.author = author   # Stores the book author
+        self.is_available = is_available   # Stores availability status
+        self.page_count = page_count   # Stores the number of pages
     
-    def __str__(self):
-        if self.is_available:
-            status = "Available"
-        else:
-            status = "Checked out"
+    def __str__(self):   # String representation of Book
+        if self.is_available:   # Checks availability
+            status = "Available"   # Sets status as Available
+        else:   # If not available
+            status = "Checked out"   # Sets status as Checked out
         
-        return self.title + " by " + self.author + " " + self.page_count + " pages [" + status + "]"
+        return self.title + " by " + self.author + " " + self.page_count + " pages [" + status + "]"   # Returns book info
 
-class DVD(MediaItem):
-    def __init__(self,title,author,duration,is_available=True):
-        super().__init__(title,author,is_available)
-        self.duration = duration
+class DVD(MediaItem):   # Subclass for DVDs
+    def __init__(self,title,author,duration,is_available=True):   # Constructor for DVD
+        super().__init__(title,author,is_available)   # Calls parent constructor
+        self.duration = duration   # Stores the DVD duration
     
-    def checkout(self):
-        print("\nHandle with care: Do not scratch the disc.")
-        return super().checkout()
+    def checkout(self):   # Overrides checkout method
+        print("\nHandle with care: Do not scratch the disc.")   # Prints warning message
+        return super().checkout()   # Calls parent checkout method
     
-    def __str__(self):
-        if self.is_available:
-            status = "Available"
-        else:
-            status = "Checked out"
+    def __str__(self):   # String representation of DVD
+        if self.is_available:   # Checks availability
+            status = "Available"   # Sets status as Available
+        else:   # If not available
+            status = "Checked out"   # Sets status as Checked out
 
-        return self.title + " by " + self.author + " " + self.duration + " minutes [" + status + "]"
+        return self.title + " by " + self.author + " " + self.duration + " minutes [" + status + "]"   # Returns DVD info
     
-class LibraryCollection:
-    def __init__(self):
-        self.items = []
+class LibraryCollection:   # Class for storing multiple items
+    def __init__(self):   # Constructor method
+        self.items = []   # Creates an empty list for items
     
-    def add_item(self,item):
-        self.items.append(item)
+    def add_item(self,item):   # Method to add an item
+        self.items.append(item)   # Appends the item to the list
     
-    def list_available(self):
-        available_items = []
-        for item in self.items:
-            if item.is_available:
-                available_items.append(item.title)
-        return available_items
+    def list_available(self):   # Method to list available items
+        available_items = []   # Creates an empty list for available titles
+        for item in self.items:   # Loops through all items
+            if item.is_available:   # Checks if the item is available
+                available_items.append(item.title)   # Adds the title to the list
+        return available_items   # Returns the list of available titles
--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-from custom_classes import MediaItem, Book, DVD, LibraryCollection
+from custom_classes import MediaItem, Book, DVD, LibraryCollection   # Imports classes from custom_classes.py
 
-library = LibraryCollection()
+library = LibraryCollection()   # Creates a library collection object
 
-book1 = Book("Her Temas Iz Birakir", "Emrah Serbes", "320")
-book2 = Book("Dogu Ekspresinde Cinayet", "Agatha Christie", "234")
-dvd1 = DVD("GORA", "Cem Yilmaz", "127")
-dvd2 = DVD("AROG", "Cem Yilmaz", "115")
+book1 = Book("Her Temas Iz Birakir", "Emrah Serbes", "320")   # Creates the first book object
+book2 = Book("Dogu Ekspresinde Cinayet", "Agatha Christie", "234")   # Creates the second book object
+dvd1 = DVD("GORA", "Cem Yilmaz", "127")   # Creates the first DVD object
+dvd2 = DVD("AROG", "Cem Yilmaz", "115")   # Creates the second DVD object
 
-library.add_item(book1)
-library.add_item(book2)
-library.add_item(dvd1)
-library.add_item(dvd2)
+library.add_item(book1)   # Adds the first book to the library
+library.add_item(book2)   # Adds the second book to the library
+library.add_item(dvd1)   # Adds the first DVD to the library
+library.add_item(dvd2)   # Adds the second DVD to the library
 
-print(book1)
-print(book2)
-print(dvd1)
-print(dvd2)
+print(book1)   # Prints the information of book1
+print(book2)   # Prints the information of book2
+print(dvd1)   # Prints the information of dvd1
+print(dvd2)   # Prints the information of dvd2
 
-print(library.list_available())
+print(library.list_available())   # Prints the list of available item titles
 
-dvd1.checkout()
-book2.checkout()
+dvd1.checkout()   # Checks out the first DVD
+book2.checkout()   # Checks out the second book
 
-print(library.list_available())
+print(library.list_available())   # Prints the list of available item titles
 
-print(book1)
-print(book2)
-print(dvd1)
-print(dvd2)
+print(book1)   # Prints the information of book1
+print(book2)   # Prints the information of book2
+print(dvd1)   # Prints the information of dvd1
+print(dvd2)   # Prints the information of dvd2
